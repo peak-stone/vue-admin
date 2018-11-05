c002614f5a9bd5d81e30c862ecdfec33c082a62f
neikvon <neikvon@hotmail.com>
2018-11-05 21:32:52 +0800

    feat(apollo): 支持文件上传客户端

--- utils/apollo.js
+++ utils/apollo.js
@@ -2,6 +2,7 @@ import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import VueApollo from 'vue-apollo'
+import { createUploadLink } from 'apollo-upload-client'
 
 let apolloProvider
 
@@ -16,12 +17,21 @@ export function initApollo (Vue) {
 export function addApolloClients (clients) {
   const _clients = Array.isArray(clients) ? clients : [clients]
   for (let client of _clients) {
-    apolloProvider.clients[client.name] = new ApolloClient({
-      link: new HttpLink({
-        uri: client.uri
-      }),
-      cache: new InMemoryCache(),
-      connectToDevTools: true
-    })
+    apolloProvider.clients[client.name] = client.upload
+      ? new ApolloClient({
+        link: createUploadLink({
+          uri: client.uri,
+          ...client.upload
+        }),
+        cache: new InMemoryCache(),
+        connectToDevTools: true
+      })
+      : new ApolloClient({
+        link: new HttpLink({
+          uri: client.uri
+        }),
+        cache: new InMemoryCache(),
+        connectToDevTools: true
+      })
   }
 }
